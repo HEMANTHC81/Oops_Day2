3b9d2ee4cdf4ff59124831bcf336b0c757a2f4d9
HEMANTHC81 <4MU22CS024@mysururoyal.org>
2026-02-03 22:12:52 +0530

    Add files via upload

---
+++ Day 2 inheritance.py
@@ -0,0 +1,24 @@
+class Book: # parent
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def display_book_details(self):
+        print("Title:", self.title)
+        print("Author:", self.author)
+
+class IssuedBook(Book): # child
+    def __init__(self, title, author, issued_to, issued_date):
+        super().__init__(title, author)
+        self.issued_to = issued_to
+        self.issued_date = issued_date
+
+    def display_issued_book_details(self):
+        self.display_book_details()
+        print("Issued To:", self.issued_to)
+        print("Issued Date:", self.issued_date)
+
+
+book1 = IssuedBook("Python Programming","Guido van Rossum","Ram","02-02-2026")
+
+book1.display_issued_book_details()
